2c760e229738d3955d68dfd3aacf665f625f949e
Guilherme Gruner Birckholz <guilherme.birckholz@pucpr.edu.br>
2024-03-13 20:43:34 -0300

    tar1

--- src/Album.java
+++ src/Album.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Album {
+import java.util.ArrayList;
+
+public class Album {
+    private String  nome;
+    private String  anoLan;
+    private ArrayList<String> listMus;
+
+    public Album(String nome, String anoLan) {
+        this.nome = nome;
+        this.anoLan = anoLan;
+        listMus = new ArrayList<>();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getAnoLan() {
+        return anoLan;
+    }
+
+    public void addMus(String nomeMus) {
+        this.listMus.add(nomeMus);
+    }
 }
